3855541d26315e0d569347d6d108d511f9b5aed4
Joshua <Joshua@Admins-MacBook.local>
2018-01-31 20:55:44 -0600

    adding resolution for api show

--- reserv_server.js
+++ reserv_server.js
@@ -36,10 +36,12 @@ app.get("/tables", function(req, res){
 })
 
 app.get("/api/tables", function(req, res){
+  console.log(res);
   return res.json(tables);
 });
 
 app.get("/api/waitlist", function(req, res){
+  console.log(res);
   return res.json(waitList);
 });
   
@@ -52,16 +54,16 @@ app.post("/reserve/new", function(req, res) {
 
   // We then add the json the user sent to the character array
   reservations.push(newReservation);
-  if(tables.length>5){
+  if(tables.length<5){
     tables.push(newReservation);
+    console.log(tables);
   }else{
     waitList.push(newReservation);
+    console.log(waitList);
   }
 
   // We then display the JSON to the users
   res.json(newReservation);
 });
 
-app.get("/api/waitlist", function(req, res){
-  return res.json(waitList);
-});
+
