f03a934b7e177d8a9b4faa09d699dc0d20536ee9
Lauryn McFarland <lauryn@Lauryns-MacBook-Pro.local>
2018-01-31 20:17:48 -0600

    add reservations

--- reserv_server.js
+++ reserv_server.js
@@ -37,11 +37,11 @@ app.get("/tables", function(req, res){
 
 app.get("/api/tables", function(req, res){
   return res.json(tables);
+})
 
-  
 var reservations = [];
 // Create New Reservation - takes in JSON input
-app.post("/reservation/new", function(req, res) {
+app.post("/reserve/new", function(req, res) {
   var newReservation = req.body;
 
   console.log(newReservation);
